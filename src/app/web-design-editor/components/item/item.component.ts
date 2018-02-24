b9ac2c83a94d1bebe14a475be23772f1a67aabba
pcboby <pcboby@126.com>
2018-02-25 00:53:37 +0800

    update rebug by css

--- src/app/web-design-editor/components/item/item.component.ts
+++ src/app/web-design-editor/components/item/item.component.ts
@@ -12,7 +12,8 @@ import { DraggableDirective } from '../../directives/draggable.directive';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ItemComponent implements OnInit {
   @Input() model: any;
